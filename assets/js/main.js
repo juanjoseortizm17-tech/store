b564eea036a61d911eb35c26913078e8bf8aa35b
TechNova <juanjoseortizm17@gmail.com>
2025-09-23 20:17:02 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+ function mostrarEscena(id) {
+      document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+      document.getElementById(id).classList.add("active");
+    }
